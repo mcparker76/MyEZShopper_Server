8625c9b7a2c5e133133e7ccd1c6a5eed12c7571b
Matt Parker <mcparker76@gmail.com>
2015-11-14 18:01:34 -0500

    Added server-side script for removing expired deals from databse using cron module.

---
+++ cronServer.js
@@ -0,0 +1,31 @@
+var cron = require('cron');
+var mongoose = require('mongoose');
+var schema = new mongoose.Schema({
+    name:"string", 
+	price:"number", storeName:"string",
+	location:"string", expirationDate:"date",
+	description:"string",category:"string"
+  });
+
+var deal = mongoose.model('Deal', schema);
+
+// Connect to database.
+mongoose.connect("localhost", "MyEZShopper");
+
+var cronJob = cron.job('00 35 17 * * 1-7', function(){
+    // perform operation e.g. GET request http.get() etc.
+	var currDate = new Date();
+	console.log("Month: " + (currDate.getMonth()+1));
+	console.log("Day: " + currDate.getDate());
+	console.log("Year: " + currDate.getFullYear());
+	var expDate = Date.parse((currDate.getMonth() + 1) + "-" + currDate.getDate() + "-" + currDate.getFullYear() + " 00:00");
+	console.log(expDate);
+	
+	deal.remove({expirationDate:expDate}, function(err){
+		if (err){ return handleError(err);console.log("ERROR");}
+		else console.log("HERE");
+	});
+	
+    console.info('cron job completed');
+}); 
+cronJob.start();
