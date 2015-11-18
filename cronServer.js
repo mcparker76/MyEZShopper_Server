4b96a641ce5e43804659f18e6601100e9a4d1d80
Matt Parker <mcparker76@gmail.com>
2015-11-17 23:12:56 -0500

    Enabled deletion of deals from databse using the mongoose find method, followed by a remove call.

--- cronServer.js
+++ cronServer.js
@@ -11,26 +11,29 @@ var schema = new mongoose.Schema({
 
 var deal = mongoose.model('Deal', schema);
 
-// Connect to database.
 mongoose.connect("localhost", "MyEZShopper");
 
-//database deletes entries from current date every 3 minutes
-var cronJob = cron.job('0 */3 * * * *', function(){
+var cronJob = cron.job('* */3 * * * *', function(){
+	
+	var t = moment();
+	console.log(moment().zone(300).format());
+	var currDate = new Date(moment().format());
 
-	var currDate = new Date();
+	//var currDate = new Date();
+	console.log("CURR DATE: " + currDate.getTime());
 	console.log("Month: " + (currDate.getMonth()+1));
 	console.log("Day: " + currDate.getDate());
 	console.log("Year: " + currDate.getFullYear());
+
+	var expDate = new Date(Date.parse((currDate.getMonth() + 1) + "-" + currDate.getDate() + "-" + currDate.getFullYear()));
 	
-	var testing = moment(currDate.getTime()).utcOffset(-300).format('YYYY-MM-DD');
-	console.log("TESTING: " + testing);
-	currDate = new Date(testing);
-	var expDate = Date.parse((currDate.getMonth() + 1) + "-" + currDate.getDate() + "-" + currDate.getFullYear());
-	console.log(expDate);
+	console.log("EXP DATE: " + expDate.toLocaleDateString());
 	
+	deal.find({expirationDate: {$lt:expDate}}).remove().exec();
+	/*
 	deal.remove({expirationDate:expDate}, function(err){
 		if (err){ return handleError(err);console.log("ERROR");}
-	});
+	});*/
 	
     console.info('cron job completed');
 }); 
