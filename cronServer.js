f4a5ceb87637755d0a4544a7f842ce9191120dd1
mcparker76 <mcparker76@gmail.com>
2015-11-18 20:07:11 -0500

    Updated system. Updated query to be dates less than current system time.

--- cronServer.js
+++ cronServer.js
@@ -13,23 +13,18 @@ var deal = mongoose.model('Deal', schema);
 
 mongoose.connect("localhost", "MyEZShopper");
 
-var cronJob = cron.job('* */3 * * * *', function(){
+var cronJob = cron.job('*/45 * * * * *', function(){
 	
-	var t = moment();
-	console.log(moment().zone(300).format());
-	var currDate = new Date(moment().format());
+	var currDate = new Date(moment().utcOffset(-300).format('MM/DD/YYYY'));
 
-	//var currDate = new Date();
 	console.log("CURR DATE: " + currDate.getTime());
 	console.log("Month: " + (currDate.getMonth()+1));
 	console.log("Day: " + currDate.getDate());
 	console.log("Year: " + currDate.getFullYear());
 
-	var expDate = new Date(Date.parse((currDate.getMonth() + 1) + "-" + currDate.getDate() + "-" + currDate.getFullYear()));
+	//var expDate = new Date(Date.parse((currDate.getMonth() + 1) + "-" + currDate.getDate() + "-" + currDate.getFullYear()));
 	
-	console.log("EXP DATE: " + expDate.toLocaleDateString());
-	
-	deal.find({expirationDate: {$lt:expDate}}).remove().exec();
+	deal.find({expirationDate: {$lt:currDate}}).remove().exec();
 	/*
 	deal.remove({expirationDate:expDate}, function(err){
 		if (err){ return handleError(err);console.log("ERROR");}
